aceca74db01a8217b3c746e4f3256646fa9ea687
tristanengst <tristanengst@gmail.com>
2022-01-06 22:08:13 -0500

    Bug testing

--- generators/camnet/models/modules/architecture.py
+++ generators/camnet/models/modules/architecture.py
@@ -228,6 +228,8 @@ class LPNet(nn.Module):
             self.lins[i].weight = torch.sqrt(weights["lin%d.model.1.weight" % i])
 
     def forward(self, in0, avg=False):
+
+        print("IN0", in0.device)
         in0 = 2 * in0 - 1
         in0_input = self.scaling_layer(in0)
         outs0 = self.net.forward(in0_input)
