8316931406a2ca9c3fa2f5d3c60536b0c00030fa
Tristan Engst <tme3@cedar1.cedar.computecanada.ca>
2022-02-13 15:01:32 -0800

    Adding fixed SLURM script

--- slurm/camnet_decorrupting.sh
+++ slurm/camnet_decorrupting.sh
@@ -2,25 +2,20 @@
 
 # The SBATCH directives must appear before any executable line in this script.
 
-#SBATCH --array=1-13
-#SBATCH --time=3-0:0:0          # Time: D-H:M:S
+#SBATCH --array=1-12
+#SBATCH --time=2-23:59:59          # Time: D-H:M:S
 #SBATCH --account=def-keli      # Account: def-keli/rrg-keli
-#SBATCH --mem=50G               # Memory in total
+#SBATCH --mem=32G               # Memory in total
 #SBATCH --nodes=1               # Number of nodes requested.
 #SBATCH --cpus-per-task=10      # Number of cores per task.
 #SBATCH --gres=gpu:v100l:2      # 32G V100
-
-# Uncomment this to have Slurm cd to a directory before running the script.
-# You can also just run the script from the directory you want to be in.
-# Change the folder below to your code directory
-#SBATCH -D /projects/def-keli/tme3/ISICLE
 
 # Uncomment to control the output files. By default stdout and stderr go to
 # the same place, but if you use both commands below they'll be split up.
 # %N is the hostname (if used, will create output(s) per node).
 # %j is jobid.
 
-#SBATCH --output=job_results/%j.txt
+#SBATCH --output=job_results/feb11_camnet_20epochs%j.txt
 
 # Below sets the email notification, swap to your email to receive notifications
 #SBATCH --mail-user=tme28@cornell.edu
@@ -54,43 +49,40 @@ echo "SLURM CHECKPOINT B"
 # Do all the research.
 if [ "$SLURM_ARRAY_TASK_ID" = 1 ]
 then
-    python TrainGenerator.py --res 32 64 --wandb 0 --data camnet3 --epochs 1 --bs 4 --mini_bs 4 --code_bs 4 --sp 128 --num_samples 128 --loss mse --gpus 0 --verbose 1 --ipcpe 3
+    python TrainGenerator.py --res 64 128 --data camnet3 --epochs 20 --bs 4 --mini_bs 4 --code_bs 4 --sp 32 --num_samples 128  --pix_mask_frac .6 --pix_mask_size 16 --gpus 0 1 --verbose 1 --ipcpe 3 --loss mse --color_space rgb --lr 1e-5
 elif [ "$SLURM_ARRAY_TASK_ID" = 2 ]
 then
-    python TrainGenerator.py --res 32 64 128 --data camnet3 --epochs 40 --bs 4 --mini_bs 4 --code_bs 4 --sp 128 --num_samples 128  --gpus 0 1 --verbose 1 --ipcpe 3 --loss mse --color_space lab --lr 1e-2
+    python TrainGenerator.py --res 64 128 256 --data camnet3 --epochs 20 --bs 4 --mini_bs 4 --code_bs 4 --sp 32 --num_samples 128  --pix_mask_frac .2 --pix_mask_size 5 --gpus 0 1 --verbose 1 --ipcpe 3 --loss mse --color_space lab --lr 1e-5
 elif [ "$SLURM_ARRAY_TASK_ID" = 3 ]
 then
-    python TrainGenerator.py --res 32 64 128 --data camnet3 --epochs 40 --bs 4 --mini_bs 4 --code_bs 4 --sp 128 --num_samples 128  --gpus 0 1 --verbose 1 --ipcpe 3 --loss mse --color_space lab --lr 1e-3
+    python TrainGenerator.py --res 64 128 256 --data camnet3 --epochs 20 --bs 4 --mini_bs 4 --code_bs 4 --sp 32 --num_samples 128  --pix_mask_frac .3 --pix_mask_size 5 --gpus 0 1 --verbose 1 --ipcpe 3 --loss mse --color_space lab --lr 1e-5
 elif [ "$SLURM_ARRAY_TASK_ID" = 4 ]
 then
-    python TrainGenerator.py --res 32 64 128 --data camnet3 --epochs 40 --bs 4 --mini_bs 4 --code_bs 4 --sp 128 --num_samples 128  --gpus 0 1 --verbose 1 --ipcpe 3 --loss mse --color_space lab --lr 1e-4
+    python TrainGenerator.py --res 64 128 256 --data camnet3 --epochs 20 --bs 4 --mini_bs 4 --code_bs 4 --sp 32 --num_samples 128  --pix_mask_frac .4 --pix_mask_size 5 --gpus 0 1 --verbose 1 --ipcpe 3 --loss mse --color_space lab --lr 1e-5
 elif [ "$SLURM_ARRAY_TASK_ID" = 5 ]
 then
-    python TrainGenerator.py --res 32 64 128 --data camnet3 --epochs 40 --bs 4 --mini_bs 4 --code_bs 4 --sp 128 --num_samples 128  --gpus 0 1 --verbose 1 --ipcpe 3 --loss mse --color_space lab --lr 1e-5
+    python TrainGenerator.py --res 64 128 256 --data camnet3 --epochs 20 --bs 4 --mini_bs 4 --code_bs 4 --sp 32 --num_samples 128  --pix_mask_frac .5 --pix_mask_size 5 --gpus 0 1 --verbose 1 --ipcpe 3 --loss mse --color_space lab --lr 1e-5
 elif [ "$SLURM_ARRAY_TASK_ID" = 6 ]
 then
-    python TrainGenerator.py --res 32 64 128 --data camnet3 --epochs 40 --bs 4 --mini_bs 4 --code_bs 4 --sp 128 --num_samples 128  --gpus 0 1 --verbose 1 --ipcpe 3 --loss lpips --color_space lab --lr 1e-2
+    python TrainGenerator.py --res 64 128 256 --data camnet3 --epochs 20 --bs 4 --mini_bs 4 --code_bs 4 --sp 32 --num_samples 128  --pix_mask_frac .6 --pix_mask_size 16 --gpus 0 1 --verbose 1 --ipcpe 3 --loss lpips --color_space rgb --lr 1e-5
 elif [ "$SLURM_ARRAY_TASK_ID" = 7 ]
 then
-    python TrainGenerator.py --res 32 64 128 --data camnet3 --epochs 40 --bs 4 --mini_bs 4 --code_bs 4 --sp 128 --num_samples 128  --gpus 0 1 --verbose 1 --ipcpe 3 --loss lpips --color_space lab --lr 1e-3
+    python TrainGenerator.py --res 64 128 --data camnet3 --epochs 20 --bs 4 --mini_bs 4 --code_bs 4 --sp 128 --num_samples 128  --pix_mask_frac .4 --pix_mask_size 5 --gpus 0 1 --verbose 1 --ipcpe 3 --loss lpips --color_space rgb --lr 1e-3
 elif [ "$SLURM_ARRAY_TASK_ID" = 8 ]
 then
-    python TrainGenerator.py --res 32 64 128 --data camnet3 --epochs 40 --bs 4 --mini_bs 4 --code_bs 4 --sp 128 --num_samples 128  --gpus 0 1 --verbose 1 --ipcpe 3 --loss lpips --color_space lab --lr 1e-4
+    python TrainGenerator.py --res 64 128 --data camnet3 --epochs 20 --bs 4 --mini_bs 4 --code_bs 4 --sp 128 --num_samples 128  --pix_mask_frac .4 --pix_mask_size 5 --gpus 0 1 --verbose 1 --ipcpe 3 --loss lpips --color_space rgb --lr 1e-4
 elif [ "$SLURM_ARRAY_TASK_ID" = 9 ]
 then
-    python TrainGenerator.py --res 32 64 128 --data camnet3 --epochs 40 --bs 4 --mini_bs 4 --code_bs 4 --sp 128 --num_samples 128  --gpus 0 1 --verbose 1 --ipcpe 3 --loss lpips --color_space lab --lr 1e-5
+    python TrainGenerator.py --res 64 128 --data camnet3 --epochs 20 --bs 4 --mini_bs 4 --code_bs 4 --sp 128 --num_samples 128  --pix_mask_frac .4 --pix_mask_size 5 --gpus 0 1 --verbose 1 --ipcpe 3 --loss lpips --color_space rgb --lr 1e-5
 elif [ "$SLURM_ARRAY_TASK_ID" = 10 ]
 then
-    python TrainGenerator.py --res 32 64 128 --data camnet3 --epochs 40 --bs 4 --mini_bs 4 --code_bs 4 --sp 128 --num_samples 128  --gpus 0 1 --verbose 1 --ipcpe 3 --loss lpips --color_space rgb --lr 1e-2
+    python TrainGenerator.py --res 64 128 256 --data camnet3 --epochs 20 --bs 4 --mini_bs 4 --code_bs 4 --sp 32 --num_samples 128  --pix_mask_frac .4 --pix_mask_size 5 --gpus 0 1 --verbose 1 --ipcpe 3 --loss lpips --color_space rgb --lr 1e-5
 elif [ "$SLURM_ARRAY_TASK_ID" = 11 ]
 then
-    python TrainGenerator.py --res 32 64 128 --data camnet3 --epochs 40 --bs 4 --mini_bs 4 --code_bs 4 --sp 128 --num_samples 128  --gpus 0 1 --verbose 1 --ipcpe 3 --loss lpips --color_space rgb --lr 1e-3
+    python TrainGenerator.py --res 32 64 128 256 --data camnet3 --epochs 20 --bs 4 --mini_bs 4 --code_bs 4 --sp 32 --num_samples 128  --pix_mask_frac .4 --pix_mask_size 5 --gpus 0 1 --verbose 1 --ipcpe 3 --loss lpips --color_space rgb --lr 1e-5
 elif [ "$SLURM_ARRAY_TASK_ID" = 12 ]
 then
-    python TrainGenerator.py --res 32 64 128 --data camnet3 --epochs 40 --bs 4 --mini_bs 4 --code_bs 4 --sp 128 --num_samples 128  --gpus 0 1 --verbose 1 --ipcpe 3 --loss lpips --color_space rgb --lr 1e-4
-elif [ "$SLURM_ARRAY_TASK_ID" = 13 ]
-then
-    python TrainGenerator.py --res 32 64 128 --data camnet3 --epochs 40 --bs 4 --mini_bs 4 --code_bs 4 --sp 128 --num_samples 128  --gpus 0 1 --verbose 1 --ipcpe 3 --loss lpips --color_space rgb --lr 1e-5
+    python TrainGenerator.py --res 32 64 128 --data camnet3 --epochs 20 --bs 4 --mini_bs 4 --code_bs 4 --sp 32 --num_samples 128  --pix_mask_frac .4 --pix_mask_size 5 --gpus 0 1 --verbose 1 --ipcpe 3 --loss lpips --color_space rgb --lr 1e-5
 else
     echo "No case here"
 fi
