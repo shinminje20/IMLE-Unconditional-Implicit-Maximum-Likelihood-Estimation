8efd111b6b5b99b12ddf5544259469cd650b9abe
tristanengst <tristanengst@gmail.com>
2022-02-13 19:49:13 -0800

    Bug fix

--- TrainGenerator.py
+++ TrainGenerator.py
@@ -450,7 +450,7 @@ if __name__ == "__main__":
         ########################################################################
         # After each epoch, log results and data
         ########################################################################
-        lr = scheduler.get_last_lr()[0],
+        lr = scheduler.get_last_lr()[0][0],
 
         tqdm.write(f"loss_tr {loss_tr:.5f} | lr {lr:.5f}")
 
