9a67999df6c8715d1336eb06bf4d77171d38493a
Tristan Engst <tristanengst@gmail.com>
2022-04-26 16:56:03 -0700

    Adding generator naming scheme and more wandb utilities

--- utils/Utils.py
+++ utils/Utils.py
@@ -27,6 +27,7 @@ torch.backends.cudnn.benchmark = True
 num_workers = 6
 
 # Turn off WandB console logging, since we don't need it and it breaks TQDM.
+os.environ["WANDB_CONSOLE"] = "off"
 
 # Make non-determinism work out. This function should be called first
 def set_seed(seed):
