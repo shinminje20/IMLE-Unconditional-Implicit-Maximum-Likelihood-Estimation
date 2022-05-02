10d361e1fa4c59f785da4cb7eea4c139ed416a75
Tristan Engst <tristanengst@gmail.com>
2022-05-01 23:43:23 -0700

    Fixing SLURM error

--- slurm/camnet3_superres.sh
+++ slurm/camnet3_superres.sh
@@ -45,27 +45,27 @@ conda activate py39ISICLE
 export PYTHONUNBUFFERED=1
 
 # Do all the research.
-if [[ $((SLURM_ARRAY_TASK_ID % 3)) == 0 ]]
+if [[ $((SLURM_ARRAY_TASK_ID % 7)) == 0 ]]
 then
-    python TrainGeneratorWandB.py --lr 5e-4 --epochs 20 --data_path ~/scratch/ISICLE/data --bs 16 --res 16 32 64 128 256 --ns 128 128 128 128 --sp 128 128 64 16 --data camnet3 --normalize 1 --gpus 0 1 --wandb offline --chunk_epochs 1 --resume $((SLURM_ARRAY_TASK_ID / 7)) --seed $SLURM_ARRAY_TASK_ID --warmup 1024
-elif [[ $((SLURM_ARRAY_TASK_ID % 3)) == 1 ]]
+    python TrainGeneratorWandB.py --lr 5e-4 --epochs 20 --data_path ~/scratch/ISICLE/data --bs 16 --res 16 32 64 128 256 --ns 128 128 128 128 --sp 128 128 64 16 --data camnet3 --normalize 1 --gpus 0 1 --wandb offline --chunk_epochs 1 --resume $((SLURM_ARRAY_TASK_ID / 7)) --seed $((SLURM_ARRAY_TASK_ID % 7)) --warmup 1024
+elif [[ $((SLURM_ARRAY_TASK_ID % 7)) == 1 ]]
 then
-    python TrainGeneratorWandB.py --lr 5e-4 --epochs 20 --data_path ~/scratch/ISICLE/data --bs 16 --res 16 32 64 128 256 --ns 128 128 128 128 --sp 128 128 64 16 --data camnet3 --normalize 1  --gpus 0 1 --wandb offline --chunk_epochs 1 --resume $((SLURM_ARRAY_TASK_ID / 7)) --seed $SLURM_ARRAY_TASK_ID --warmup 1024 --grayscale .5
-elif [[ $((SLURM_ARRAY_TASK_ID % 3)) == 2 ]]
+    python TrainGeneratorWandB.py --lr 5e-4 --epochs 20 --data_path ~/scratch/ISICLE/data --bs 16 --res 16 32 64 128 256 --ns 128 128 128 128 --sp 128 128 64 16 --data camnet3 --normalize 1  --gpus 0 1 --wandb offline --chunk_epochs 1 --resume $((SLURM_ARRAY_TASK_ID / 7)) --seed $((SLURM_ARRAY_TASK_ID % 7)) --warmup 1024 --grayscale .5
+elif [[ $((SLURM_ARRAY_TASK_ID % 7)) == 2 ]]
 then
-    python TrainGeneratorWandB.py --lr 5e-4 --epochs 20 --data_path ~/scratch/ISICLE/data --bs 16 --res 16 32 64 128 256 --ns 128 128 128 128 --sp 128 128 64 16 --data camnet3 --gpus 0 1 --wandb offline --chunk_epochs 1 --resume $((SLURM_ARRAY_TASK_ID / 7)) --seed $SLURM_ARRAY_TASK_ID --warmup 1024
-elif [[ $((SLURM_ARRAY_TASK_ID % 3)) == 3 ]]
+    python TrainGeneratorWandB.py --lr 5e-4 --epochs 20 --data_path ~/scratch/ISICLE/data --bs 16 --res 16 32 64 128 256 --ns 128 128 128 128 --sp 128 128 64 16 --data camnet3 --gpus 0 1 --wandb offline --chunk_epochs 1 --resume $((SLURM_ARRAY_TASK_ID / 7)) --seed $((SLURM_ARRAY_TASK_ID % 7)) --warmup 1024
+elif [[ $((SLURM_ARRAY_TASK_ID % 7)) == 3 ]]
 then
-    python TrainGeneratorWandB.py --lr 5e-4 --epochs 20 --data_path ~/scratch/ISICLE/data --bs 16 --res 16 32 64 128 256 --ns 128 128 128 128 --sp 128 128 64 16 --data camnet3 --gpus 0 1 --wandb offline --chunk_epochs 1 --resume $((SLURM_ARRAY_TASK_ID / 7)) --seed $SLURM_ARRAY_TASK_ID --warmup 1024 --grayscale .5
-elif [[ $((SLURM_ARRAY_TASK_ID % 3)) == 4 ]]
+    python TrainGeneratorWandB.py --lr 5e-4 --epochs 20 --data_path ~/scratch/ISICLE/data --bs 16 --res 16 32 64 128 256 --ns 128 128 128 128 --sp 128 128 64 16 --data camnet3 --gpus 0 1 --wandb offline --chunk_epochs 1 --resume $((SLURM_ARRAY_TASK_ID / 7)) --seed $((SLURM_ARRAY_TASK_ID % 7)) --warmup 1024 --grayscale .5
+elif [[ $((SLURM_ARRAY_TASK_ID % 7)) == 4 ]]
 then
-    python TrainGeneratorWandB.py --lr 1e-4 --epochs 20 --data_path ~/scratch/ISICLE/data --bs 16 --res 16 32 64 128 256 --ns 128 128 128 128 --sp 128 128 64 16 --data camnet3 --gpus 0 1 --wandb offline --chunk_epochs 1 --resume $((SLURM_ARRAY_TASK_ID / 7)) --seed $SLURM_ARRAY_TASK_ID --warmup 1024
-elif [[ $((SLURM_ARRAY_TASK_ID % 3)) == 5 ]]
+    python TrainGeneratorWandB.py --lr 1e-4 --epochs 20 --data_path ~/scratch/ISICLE/data --bs 16 --res 16 32 64 128 256 --ns 128 128 128 128 --sp 128 128 64 16 --data camnet3 --gpus 0 1 --wandb offline --chunk_epochs 1 --resume $((SLURM_ARRAY_TASK_ID / 7)) --seed $((SLURM_ARRAY_TASK_ID % 7)) --warmup 1024
+elif [[ $((SLURM_ARRAY_TASK_ID % 7)) == 5 ]]
 then
-    python TrainGeneratorWandB.py --lr 5e-5 --epochs 20 --data_path ~/scratch/ISICLE/data --bs 16 --res 16 32 64 128 256 --ns 128 128 128 128 --sp 128 128 64 16 --data camnet3 --gpus 0 1 --wandb offline --chunk_epochs 1 --resume $((SLURM_ARRAY_TASK_ID / 7)) --seed $SLURM_ARRAY_TASK_ID --warmup 1024
-elif [[ $((SLURM_ARRAY_TASK_ID % 3)) == 6 ]]
+    python TrainGeneratorWandB.py --lr 5e-5 --epochs 20 --data_path ~/scratch/ISICLE/data --bs 16 --res 16 32 64 128 256 --ns 128 128 128 128 --sp 128 128 64 16 --data camnet3 --gpus 0 1 --wandb offline --chunk_epochs 1 --resume $((SLURM_ARRAY_TASK_ID / 7)) --seed $((SLURM_ARRAY_TASK_ID % 7)) --warmup 1024
+elif [[ $((SLURM_ARRAY_TASK_ID % 7)) == 6 ]]
 then
-    python TrainGeneratorWandB.py --lr 1e-5 --epochs 20 --data_path ~/scratch/ISICLE/data --bs 16 --res 16 32 64 128 256 --ns 128 128 128 128 --sp 128 128 64 16 --data camnet3 --gpus 0 1 --wandb offline --chunk_epochs 1 --resume $((SLURM_ARRAY_TASK_ID / 7)) --seed $SLURM_ARRAY_TASK_ID --warmup 1024
+    python TrainGeneratorWandB.py --lr 1e-5 --epochs 20 --data_path ~/scratch/ISICLE/data --bs 16 --res 16 32 64 128 256 --ns 128 128 128 128 --sp 128 128 64 16 --data camnet3 --gpus 0 1 --wandb offline --chunk_epochs 1 --resume $((SLURM_ARRAY_TASK_ID / 7)) --seed $((SLURM_ARRAY_TASK_ID % 7)) --warmup 1024
 else
     echo "No case here"
 fi
