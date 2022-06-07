f9eaa7952abb7311f11c228d34886449be8bf85c
Tristan Engst <tristanengst@gmail.com>
2022-06-07 03:06:51 -0700

    Adding file for better WandB syncing

--- WandBSync.py
+++ WandBSync.py
@@ -3,7 +3,7 @@ from tqdm import tqdm
 
 files = [f"wandb/{f}" for f in os.listdir("wandb") if f.startswith("offline")]
 for f in tqdm(files):
-    os.system(f"wandb sync {f} > temp.txt") # The documentation of subprocess is gross
+    os.system(f"wandb sync {f} > wandb_sync_results.txt") # The documentation of subprocess is gross
     
     with open("wandb_sync_results.txt", "r") as f:
         result = f.read()
