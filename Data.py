02620d0526a0242ec2bc0eaabd812eacebc30472
Minjae Shin <minjaeshin@cs-apex-03s.cmpt.sfu.ca>
2022-09-01 23:12:30 -0700

    image data fixed

--- Data.py
+++ Data.py
@@ -495,7 +495,6 @@ class IMLEDataset(Dataset):
         self.transform = transforms.Compose([
                                                 transforms.RandomHorizontalFlip(),
                                                 transforms.ToTensor(),
-                                                transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
                                             ])
 
         ########################################################################
